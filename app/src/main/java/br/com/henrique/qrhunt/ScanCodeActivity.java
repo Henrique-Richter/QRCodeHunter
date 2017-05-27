82a436d67754a09b364de78e482be33bc092eece
Henrique <henrique-richter@hotmail.com>
2017-05-27 20:11:09 -0300

    Scan and starting share pdf

--- app/src/main/java/br/com/henrique/qrhunt/ScanCodeActivity.java
+++ app/src/main/java/br/com/henrique/qrhunt/ScanCodeActivity.java
@@ -3,11 +3,15 @@ package br.com.henrique.qrhunt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONObject;
 
 public class ScanCodeActivity extends AppCompatActivity {
 
@@ -30,7 +34,19 @@ public class ScanCodeActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
-            // handle scan result
+            TextView scanResponse = (TextView) findViewById(R.id.scanResult);
+
+            try{
+                JSONObject obj = new JSONObject(scanResult.getContents());
+                if(obj.has("id")){
+                    scanResponse.setText("Sucesso");
+                }else{
+                    scanResponse.setText("Invalido");
+                }
+            } catch (Throwable t) {
+                Log.e("My App", "Could not parse malformed JSON: ");
+            }
+
         }
         // else continue with any other code you need in the method
 
